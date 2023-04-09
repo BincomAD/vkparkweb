17c07151fdc0650532761a529cfcb9066bec0616
BincomAD <100017314+BincomAD@users.noreply.github.com>
2023-04-09 15:07:11 +0300

    dz2full

--- askme/asker/views.py
+++ askme/asker/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def index(request):
     questions = []
     for i in range(1, 30):
         questions.append({
             'title': 'title ' + str(i),
             'id': i,
-            'text': 'text' + str(i)
+            'text': 'text' + str(i),
+            'tags': ["black_jack", "black_jack"]
         })
-    context = {'questions': questions}
+    objects = paginate(questions, request, 5)
+    context = {'questions': objects}
     return render(request, 'index.html', context)
 
 def hot(request):
@@ -17,8 +20,10 @@ def hot(request):
             'title': 'title ' + str(i),
             'id': i,
             'text': 'text' + str(i),
+            'tags': ["black_jack", "black_jack"]
         })
-    context = {'questions': questions}
+    objects = paginate(questions, request, 5)
+    context = {'questions': objects}
     return render(request, 'hot.html', context)
 
 def tag(request, tag_name):
@@ -28,8 +33,10 @@ def tag(request, tag_name):
             'title': 'title ' + str(i),
             'id': i,
             'text': 'text' + str(i),
+            'tags': ["black_jack", "black_jack"]
         })
-    context = {'questions': questions, 'tag_name': tag_name}
+    objects = paginate(questions, request, 5)
+    context = {'questions': objects, 'tag_name': tag_name}
     return render(request, 'tag.html', context)
 
 def question(request, question_id):
@@ -37,14 +44,16 @@ def question(request, question_id):
         'title': 'title ' + str(question_id),
         'id': question_id,
         'text': 'text' + str(question_id),
-        'answers': []
+        'answers': [],
+        'tags': ["black_jack","black_jack"]
     }
-    for i in range(1, 6):
+    for i in range(1, 21):
         question['answers'].append({
             'text': 'answer ' + str(i),
             'id': i,
         })
-    context = {'question': question}
+        objects = paginate(question['answers'], request, 5)
+    context = {'question': question, 'questions': objects}
     return render(request, 'question.html', context)
 
 def ask(request):
@@ -58,3 +67,14 @@ def registration(request):
 
 def settings(request):
     return render(request,'settings.html')
+
+def paginate(objects_list, request, per_page=10):
+    paginator = Paginator(objects_list, per_page)
+    page = request.GET.get('page')
+    try:
+        objects = paginator.page(page)
+    except PageNotAnInteger:
+        objects = paginator.page(1)
+    except EmptyPage:
+        objects = paginator.page(paginator.num_pages)
+    return objects
